180a228496564528e665783a4353552bbd30e924
SebastianBessoudo <sbessoud@calpoly.edu>
2025-02-17 00:00:38 -0800

    Edits and final edit for today for follow movement script
    Thought I could have the enemy jump onto the platform towards the player but its too complicated. So instead the enemy will follow the player if it is in eyesight horizontally. I put in a Patrol() command but haven't done it yet

--- Assets/Scripts/FollowMovement.cs
+++ Assets/Scripts/FollowMovement.cs
@@ -6,18 +6,41 @@ public class FollowMovement : MonoBehaviour
     private float Dis;
     [SerializeField] private float speed; 
     [SerializeField] private float distance;
+    [SerializeField] private float heightTolerance = 1f;
+
+    [SerializeField] private float patrolDistance = 3f;
 
     void Update()
     {
-        Dis = Vector2.Distance(transform.position, Target.transform.position);
         
-        if(Dis >= distance)
+        if (Target.transform.position.y - transform.position.y <= heightTolerance)
+        {
+            FollowPlayer();
+        }
+        
+        else
+        {
+            Patrol();
+        }
+    }
+
+    void FollowPlayer()
+    {
+        Dis = Vector2.Distance(transform.position, Target.transform.position);
+
+        if (Dis >= distance)
         {
             transform.position = Vector2.MoveTowards(transform.position, Target.transform.position, speed * Time.deltaTime);
         }
+
         else
         {
             transform.position = Vector2.MoveTowards(transform.position, Target.transform.position, 1 * Time.deltaTime);
         }
+    }
+
+    void Patrol()
+    {
+
     }
 }
