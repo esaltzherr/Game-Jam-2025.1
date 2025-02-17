2f787b50a681799812247f9a7e55d8e807008ed9
SebastianBessoudo <sbessoud@calpoly.edu>
2025-02-16 22:33:50 -0800

    edit follow Movement script
    edited so that the enemy follows player but slows down at specified distance with variable "distance"

--- Assets/Scripts/FollowMovement.cs
+++ Assets/Scripts/FollowMovement.cs
@@ -3,10 +3,21 @@ using UnityEngine;
 public class FollowMovement : MonoBehaviour
 {
     public GameObject Target;
+    private float Dis;
     [SerializeField] private float speed; 
+    [SerializeField] private float distance;
 
     void Update()
     {
-        transform.position = Vector2.MoveTowards(transform.position, Target.transform.position, speed * Time.deltaTime);
+        Dis = Vector2.Distance(transform.position, Target.transform.position);
+        
+        if(Dis >= distance)
+        {
+            transform.position = Vector2.MoveTowards(transform.position, Target.transform.position, speed * Time.deltaTime);
+        }
+        else
+        {
+            transform.position = Vector2.MoveTowards(transform.position, Target.transform.position, 1 * Time.deltaTime);
+        }
     }
 }
