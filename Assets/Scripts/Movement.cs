70465f3bc9ac6087e7d1ac35bc44411ffcdb5e0b
SebastianBessoudo <sbessoud@calpoly.edu>
2025-02-16 21:54:52 -0800

    updated movement script
    Character can move with direction keys and can jump with space bar. Can edit on inspection bar with speed and jump

--- Assets/Scripts/Movement.cs
+++ Assets/Scripts/Movement.cs
@@ -1,6 +1,37 @@
 using UnityEngine;
 
-public class Movement
+public class Movement : MonoBehaviour
 {
-    
+    [SerializeField] private float speed; 
+    [SerializeField] private float jump; 
+    private Rigidbody2D body;
+    private bool grounded;
+
+    private void Awake()
+    {
+        body = GetComponent<Rigidbody2D>();
+    }
+
+    private void Update()
+    {
+        body.linearVelocity = new Vector2(Input.GetAxis("Horizontal") * speed, body.linearVelocity.y);
+        if(Input.GetKey(KeyCode.Space) && grounded)
+        {
+            Jump();
+        }
+    }
+
+    private void Jump()
+    {
+        body.linearVelocity = new Vector2(body.linearVelocity.x, jump);
+        grounded = false;
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if(collision.gameObject.tag == "Ground")
+        {
+            grounded = true;
+        }
+    }
 }
