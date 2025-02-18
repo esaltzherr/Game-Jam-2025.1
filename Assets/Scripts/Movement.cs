e3c2546c5b2d22a2cc569e65ea84ea6a950698a5
SebastianBessoudo <sebastian.bessoudo@yahoo.com>
2025-02-18 14:25:29 -0800

    crouch ability
    player can now crouch under obstacles. cannot jump while crouching. Will add more constraints like can't sprint while crouching, making sure the character isn't stuck between obstacles while crouching, and the sliding ability

--- Assets/Scripts/Movement.cs
+++ Assets/Scripts/Movement.cs
@@ -6,6 +6,8 @@ public class Movement : MonoBehaviour
     [SerializeField] private float sprintMultiplier; 
     [SerializeField] private float jump; 
     [SerializeField] private float bounceForce;
+    [SerializeField] private float shrinkScaleY; // New Y scale when shrinking
+    [SerializeField] private float normalScaleY; // Normal Y scale
 
     private Rigidbody2D body;
     private bool grounded;
@@ -14,11 +16,16 @@ public class Movement : MonoBehaviour
     private bool isBouncing = false;
     private bool running = false;
 
+    private Vector3 originalScale;
+    private bool isShrinking = false;
+
     private void Awake()
     {
         body = GetComponent<Rigidbody2D>();
         body.isKinematic = false; 
         body.freezeRotation = true;
+
+        originalScale = transform.localScale;
     }
 
     private void Update()
@@ -33,30 +40,54 @@ public class Movement : MonoBehaviour
             return; // Ignore movement input while bouncing
         }
 
+        if (Input.GetKey(KeyCode.S) && grounded)
+        {
+            isShrinking = true;
+        }
+        else
+        {
+            isShrinking = false;
+        }
+
+        // Crouch
+        if (isShrinking)
+        {
+            transform.localScale = new Vector3(originalScale.x, shrinkScaleY, originalScale.z);
+        }
+        else
+        {
+            transform.localScale = new Vector3(originalScale.x, normalScaleY, originalScale.z);
+        }
+
         float moveSpeed = speed;
 
+        //Sprinting
         if (grounded && Input.GetKey(KeyCode.LeftShift))
         {
             Debug.Log("Running!");
             running = true;
             moveSpeed *= sprintMultiplier;
         }
+        //Sprinting and Jumping
         else if (!grounded && running && Input.GetKey(KeyCode.LeftShift))
         {
             Debug.Log("Running and Jumping");
             running = true;
             moveSpeed *= sprintMultiplier;
         }
+        //Walking
         else
         {
             Debug.Log("Walking");
             running = false;
         }
 
+        //Moving left and moving right
         float moveInput = Input.GetAxisRaw("Horizontal"); 
         body.linearVelocity = new Vector2(moveInput * moveSpeed, body.linearVelocity.y);
         
-        if(Input.GetKey(KeyCode.Space) && grounded)
+        //Jump
+        if((Input.GetKey(KeyCode.Space) || Input.GetKey(KeyCode.W)) && grounded && !isShrinking)
         {
             Jump();
         }
