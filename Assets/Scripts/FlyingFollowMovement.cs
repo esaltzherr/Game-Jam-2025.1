dfccaeb6d2dd4a17df11eb71326f45327ecf3ca9
SebastianBessoudo <sbessoud@calpoly.edu>
2025-02-17 17:15:55 -0800

    attempting to do a bounce effect on flying enemy
    trying to make it so when the enemy comes in contact with player, the enemy bounces off the player. Added in some extra code to make sure enemy doesn't fly off like crazy when player hits the flying enemy

--- Assets/Scripts/FlyingFollowMovement.cs
+++ Assets/Scripts/FlyingFollowMovement.cs
@@ -4,27 +4,48 @@ public class FlyingFollowMovement : MonoBehaviour
 {
     public GameObject Target;
     private float Dis;
+
     [SerializeField] private float speed; 
     [SerializeField] private float distance;
+    [SerializeField] private float slowSpeed;
+    [SerializeField] private float bounceForce;
+
+    private Rigidbody2D rb;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+        rb = GetComponent<Rigidbody2D>();
+        rb.isKinematic = false; 
+        rb.gravityScale = 0;    // No gravity for flying
+        rb.freezeRotation = true;// Prevents physics forces from affecting it
     }
 
     // Update is called once per frame
     void Update()
     {
-        Dis = Vector2.Distance(transform.position, Target.transform.position);
+        Vector2 targetPosition = Target.transform.position;
+        Dis = Vector2.Distance(rb.position, Target.transform.position);
 
-        if (Dis >= distance)
+        float currentSpeed = (Dis >= distance) ? speed : slowSpeed;
+
+        Vector2 direction = (targetPosition- rb.position).normalized;
+
+        rb.linearVelocity = direction * currentSpeed;
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        
+        if (collision.gameObject.CompareTag("Ground"))
         {
-            transform.position = Vector2.MoveTowards(transform.position, Target.transform.position, speed * Time.deltaTime);
+            // Slight bounce effect when hitting ground
+            rb.linearVelocity *= 0.5f; 
         }
-
-        else
+        if (collision.gameObject.tag == "Player")
         {
-            transform.position = Vector2.MoveTowards(transform.position, Target.transform.position, 1 * Time.deltaTime);
+            // Slight bounce effect when hitting character
+            Vector2 bounceDirection = collision.contacts[0].normal * bounceForce; 
+            rb.AddForce(bounceDirection, ForceMode2D.Impulse);
         }
     }
 }
